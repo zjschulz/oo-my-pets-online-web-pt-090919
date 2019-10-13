2089ba343dce86a4163d82279e56ba56473da45b
Zachary Schulz <zjschulz@gmail.com>
2019-10-13 18:55:59 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,27 @@
 class Cat
-  # code goes here
+  
+  attr_reader :name, :owner, :mood
+  
+  @@all = []
+  
+  def initialize(name, owner, mood = "nervous")
+    @name = name
+    @owner = owner
+    @mood = mood
+    @@all << self
+  end
+  
+  def owner=(name)
+    new_owner = Owner.new(name)
+    @owner = new_owner.name
+  end
+  
+  def mood=(feel)
+    @mood = feel
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,61 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  
+ attr_reader :name, :species
+ 
+ @@all = []
+ 
+ def initialize(name)
+   @name = name
+   @species = "human"
+   @@all << self
+ end
+ 
+ def self.all
+   @@all
+ end
+ 
+ def self.count
+   @@all.size
+ end
+ 
+ def self.reset_all
+   @@all.clear
+ end
+ 
+ def say_species
+   "I am a #{@species}."
+ end
+ 
+  def cats
+    Cat.all.select do |cats|
+      cats if cats.owner.name == self.name
+    end
+  end
+
+  def dogs 
+  end
+  
+  def buy_cat(name)
+    
+  end
+  
+  def buy_dog(name)
+    
+  end
+
+  def walk_dogs
+    
+  end
+
+  def feed_cats
+  end
+  
+  def sell_pets
+  end
+
+  def list_pets
+  end
+
 end
